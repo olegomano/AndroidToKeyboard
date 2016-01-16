a6412075ae51209c61a0cf36326a3a8bc78a013b
oleg@laptop <o.tolstov1569@gmail.com>
2016-01-15 21:58:07 -0800

    Confirmed communication from PC to Android

--- Android/KeyboardApp/app/src/main/java/calibrationapp/spectoccular/com/keyboardtolinux/MainActivity.java
+++ Android/KeyboardApp/app/src/main/java/calibrationapp/spectoccular/com/keyboardtolinux/MainActivity.java
@@ -3,35 +3,35 @@ package calibrationapp.spectoccular.com.keyboardtolinux;
 import android.hardware.usb.UsbAccessory;
 import android.hardware.usb.UsbManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.os.ParcelFileDescriptor;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.TextView;
 
-import java.io.FileDescriptor;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+
 public class MainActivity extends AppCompatActivity {
     private TextView accessoryName;
+    private TextView fromDevice;
     private UsbAccessory accessory;
     private OutputStream toAccessory;
     private InputStream  fromAccessory;
     private boolean threadRunning = true;
+    private Handler mainThreadHandler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mainThreadHandler = new Handler(Looper.getMainLooper());
         setContentView(R.layout.assessory_layout);
         accessoryName = (TextView) findViewById(R.id.accessory_name);
+        fromDevice = (TextView) findViewById(R.id.fromAccessory);
         UsbManager usbManager = (UsbManager) getSystemService(USB_SERVICE);
         UsbAccessory[] mComputer = usbManager.getAccessoryList();
         if(mComputer == null){
@@ -48,29 +48,38 @@ public class MainActivity extends AppCompatActivity {
         fromAccessory = new FileInputStream(mFileDescriptor.getFileDescriptor());
         accessoryName.setText(toAccessory.toString());
         ThreadRunable runable = new ThreadRunable();
-        runable.send = toAccessory;
-        runable.get = fromAccessory;
         new Thread(runable,"Acessory Send Thread").start();
 
     }
+    private String pendingText = "";
+    private class SetOutputText implements Runnable{
+        @Override
+        public void run() {
+            fromDevice.setText(pendingText);
+        }
+    }
 
     private class ThreadRunable implements Runnable{
-        OutputStream send;
-        InputStream get;
         @Override
         public void run() {
             while(threadRunning){
                 byte[] read = new byte[1024];
-                if(get == null){
+                if(fromAccessory == null){
                     threadRunning = false;
                     System.exit(1);
                 }
                 try {
-                    get.read(read);
+                    fromAccessory.read(read);
+                    pendingText = new String(read);
+                    mainThreadHandler.post(new SetOutputText());
+                    Thread.sleep(300);
                 } catch (IOException e) {
                     threadRunning = false;
                     System.exit(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
+
             }
 
         }
